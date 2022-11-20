05b3b4fd461781ea1a9afbf30969165bb2ef6c24
Mykola <kolanuch13@gmail.com>
2022-11-20 12:44:19 +0200

    Final commit.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,8 +1,5 @@
 import _ from 'lodash';
 
-console.log("1777");
-console.log("1777");
-
 const form = document.querySelector('form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
@@ -16,25 +13,11 @@ let feedback = {
 checkStorage();
 
 form.addEventListener('input', _.throttle(() => {
-    // feedback = {
-    //     userEmail: email.value,
-    //     userMessage: message.value,
-    // }
     localStorage.setItem("feedback-form-state", JSON.stringify({
         userEmail: email.value,
         userMessage: message.value,
     }));
 }))
-
-// email.addEventListener('input', _.throttle(() => {
-//     feedback.userEmail = email.value;
-//     localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
-// }, 500));
-
-// message.addEventListener('input', _.throttle(() => {
-//     feedback.userMessage = message.value;
-//     localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
-// }, 500));
 
 button.addEventListener('click', (event) => {
     event.preventDefault();
