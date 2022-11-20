3c34bb464689b58300cffef1ab76f6daa6060578
Mykola <kolanuch13@gmail.com>
2022-11-20 12:40:14 +0200

    linear commit.

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 
+console.log("1777");
+console.log("1777");
+
+const form = document.querySelector('form');
 const email = document.querySelector('input[name="email"]');
 const message = document.querySelector('textarea[name="message"]');
 const button = document.querySelector('button');
@@ -11,15 +15,26 @@ let feedback = {
 
 checkStorage();
 
-email.addEventListener('input', _.throttle(() => {
-    feedback.userEmail = email.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
-}, 500));
+form.addEventListener('input', _.throttle(() => {
+    // feedback = {
+    //     userEmail: email.value,
+    //     userMessage: message.value,
+    // }
+    localStorage.setItem("feedback-form-state", JSON.stringify({
+        userEmail: email.value,
+        userMessage: message.value,
+    }));
+}))
 
-message.addEventListener('input', _.throttle(() => {
-    feedback.userMessage = message.value;
-    localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
-}, 500));
+// email.addEventListener('input', _.throttle(() => {
+//     feedback.userEmail = email.value;
+//     localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
+// }, 500));
+
+// message.addEventListener('input', _.throttle(() => {
+//     feedback.userMessage = message.value;
+//     localStorage.setItem("feedback-form-state", JSON.stringify(feedback));
+// }, 500));
 
 button.addEventListener('click', (event) => {
     event.preventDefault();
